a0cbb0c80d8fefa2035629e2de078e187c880357
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-08-12 19:54:00 -0300

    no message

--- src/actions/Study/get-studies-by-idUser.action.ts
+++ src/actions/Study/get-studies-by-idUser.action.ts
@@ -4,6 +4,6 @@ import { Study } from "@/types/Study/Study";
 
 export const getStudiesByUserId = async (id: number) => {
     await sleep(2);
-    const { data } = await axiosInstance.get<Study[]>(`Study/byPatient/${id}`);
+    const { data } = await axiosInstance.get<Study[]>(`Study/byUser/${id}`);
     return data;
 }
