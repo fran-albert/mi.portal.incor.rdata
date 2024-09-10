e5e0d7cfc73214d38bf0bf0621032a4bad6daeb4
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-09-09 21:36:52 -0300

    no message

--- src/app/(protected)/usuarios/medicos/[slug]/laboratorios/page.tsx
+++ src/app/(protected)/usuarios/medicos/[slug]/laboratorios/page.tsx
@@ -8,6 +8,7 @@ import BreadcrumbComponent from "@/components/Breadcrumb";
 import { useLab } from "@/hooks/Labs/useLab";
 import { useDoctor } from "@/hooks/Doctor/useDoctor";
 import LabCard from "@/components/Laboratories/Card/card";
+import { useStudy } from "@/hooks/Study/useStudy";
 
 const DoctorLaboratoriosPage = () => {
   const params = useParams();
@@ -21,9 +22,16 @@ const DoctorLaboratoriosPage = () => {
     id,
   });
 
+  const { studiesByUserId = [], isLoadingStudiesByUserId } = useStudy({
+    idUser: id,
+    fetchStudiesByUserId: true,
+  });
+
+  const studyIds = studiesByUserId.map((study) => study.id);
+
   const { labsDetails, isLoadingLabsDetails } = useLab({
     fetchLabsDetails: true,
-    idUser: id,
+    idStudy: studyIds,
   });
 
   const breadcrumbItems = [
@@ -42,20 +50,32 @@ const DoctorLaboratoriosPage = () => {
     <>
       {error && (
         <div className="text-red-500">
-          Hubo un error al cargar los laboratorios del médico.
+          Hubo un error al cargar los laboratorios del paciente.
         </div>
       )}
-      {(isLoading || isLoadingAuth || isLoadingLabsDetails) && (
+      {(isLoading ||
+        isLoadingAuth ||
+        isLoadingLabsDetails ||
+        isLoadingStudiesByUserId) && (
         <div className="flex flex-col items-center justify-center h-screen">
           <Loading isLoading={true} />
         </div>
       )}
-      {!isLoading && !isLoadingAuth && !isLoadingLabsDetails && (
-        <div className="container space-y-2 mt-2">
-          <BreadcrumbComponent items={breadcrumbItems} />
-          {<LabCard labsDetails={labsDetails} role="médico" />}
-        </div>
-      )}
+      {!isLoading &&
+        !isLoadingAuth &&
+        !isLoadingLabsDetails &&
+        !isLoadingStudiesByUserId && (
+          <div className="container space-y-2 mt-2">
+            <BreadcrumbComponent items={breadcrumbItems} />
+            {
+              <LabCard
+                labsDetails={labsDetails}
+                studiesByUserId={studiesByUserId}
+                role="paciente"
+              />
+            }
+          </div>
+        )}
     </>
   );
 };
